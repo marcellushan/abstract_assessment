1c2f3dcb4ad39a2bf99f2d6ea9b71d641b084391
Marc Hannah <mhannah@highlands.edu>
2017-10-31 13:14:46 -0400

    edit assessor

--- resources/views/assessor/edit.blade.php
+++ resources/views/assessor/edit.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.admin') @section('content')
 <h2>Edit Assessor</h2>
-{{Form::open(['action' => ['AssessorController@removeTeam',  'method' => 'post']])}}
-<input type="hidden" name="assessor_id" value="{{$record->id}}">
 <h3>
+    {{Form::open(['action' => ['AssessorController@update', $record->id],  'method' => 'put'])}}
     <div class="row">
         <div class="col-md-6">
             <label>Name</label>
@@ -13,6 +12,11 @@
             <input type="text" name= 'username' class="form-control" value="{{$record->username}}">
         </div>
     </div>
+    <br>
+    <button type="submit" class="btn btn-primary btn-lg">Update Assessor</button>
+    </h3>
+    {{Form::open(['action' => ['AssessorController@removeTeam',  'method' => 'post']])}}
+    <input type="hidden" name="assessor_id" value="{{$record->id}}">
     <h3>Assessor is a member of the following team(s):</h3>
     <div class="row">
     @forelse($teams as $team)
